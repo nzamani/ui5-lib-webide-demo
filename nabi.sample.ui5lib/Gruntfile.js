78bbf3cf6b70df06dc42fdbd72c1c4710fad45f9
Nabi Zamani <nabizamani@aol.com>
2017-11-17 21:20:43 +0100

    lib project: improve gruntfile

--- nabi.sample.ui5lib/Gruntfile.js
+++ nabi.sample.ui5lib/Gruntfile.js
@@ -1,47 +1,75 @@
 module.exports = function(grunt) {
 	"use strict";
 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-	
+
+	var myLibPrefix = "nabi/sample/ui5lib";	// change this as needed
+
 	grunt.config.merge({
-		
+		dir:{
+			libPrefix : myLibPrefix,
+			webapp : "webapp/" + myLibPrefix
+		},
 		openui5_preload : {
-			//we don't want "nabi.sample.ui5lib" as a prefix in Component-preload.js
+			//override config from bestpractice build
 			preloadDbg : {
 				options : {
 					resources : {
-						prefix : ""
+						prefix : "<%= dir.libPrefix %>"	//use own prefix instead of name of root folder of this project
 					}
 				}
 			},
-			//we don't want "nabi.sample.ui5lib" as a prefix in Component-preload.js
+			//override config from bestpractice build
 			preloadTmp : {
 				options : {
 					resources : {
-						prefix : ""
-					}
+						src : [
+							"**/*.js",
+							"**/*.fragment.html",
+							"**/*.fragment.json",
+							"**/*.fragment.xml",
+							"**/*.view.html",
+							"**/*.view.json",
+							"**/*.view.xml",
+							"**/*.properties",
+							"**/manifest.json",		//needed because we want the manifest to be inside the Component-preload.js file
+							"!test/**"
+						],
+						prefix : "<%= dir.libPrefix %>"	//use own prefix instead of name of root folder of this project
+					},
+					compress : true
 				}
 			},
-			//we want both library-dbg.js + library-preload.js 
+			//we want both library-dbg.js + library-preload.js
 			library : {
-				options : {
-					resources: "webapp",
-					dest : "dist",
-					compatVersion : "1.48"
+				options: {
+					resources: {
+						cwd: "<%= dir.webapp %>",
+						src: [				// everything incl. library.js + any manifest.json
+							"**/*.js",
+							"**/*.fragment.html",
+							"**/*.fragment.json",
+							"**/*.fragment.xml",
+							"**/*.view.html",
+							"**/*.view.json",
+							"**/*.view.xml",
+							"**/*.properties",
+							"**/manifest.json",
+							"!test/**",
+							"!thirdparty/**"
+						],
+						prefix: "<%= dir.libPrefix %>"
+					},
+					compress: true,
+					dest: "<%= dir.dist %>"
 				},
-				libraries : {
-					"nabi/sample/ui5lib" :[
-						"nabi/sample/ui5lib/**",
-						"!nabi/sample/ui5lib/thirdparty/**"
-					]
-				}
+				libraries : true
 			}
 		}
 	});
-	
-	// good for debugging
+
 	grunt.log.writeln("\n### grunt.config() ###\n" + JSON.stringify(grunt.config(), null, 2));
-	
-	
+
+
 	grunt.registerTask("default", [
 		"clean",
 		"lint",
